96637d4e3650e52cbf9054dcbd17ecbf00139431
listenzcc <listenzcc@mail.bnu.edu.cn>
2023-11-29 21:24:39 +0800

    Delight the eeg sensors

--- src/raw.js
+++ src/raw.js
@@ -8,12 +8,23 @@ import { FontLoader } from 'three/addons/loaders/FontLoader.js';
 
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.8.5/+esm'
 import normals from 'https://cdn.jsdelivr.net/npm/angle-normals@1.0.0/+esm'
+import { createNoise4D } from 'https://cdn.jsdelivr.net/npm/simplex-noise@4.0.1/+esm'
+
+console.log(createNoise4D)
+console.log(mlMatrix)
 
 
 {
+    let colormap = d3.interpolateRdBu,
+        noise4D = createNoise4D(),
+        { Matrix } = mlMatrix;
+
+    console.log(noise4D(0.2, 0.3, 0.4, 2.0))
+
     const container = document.getElementById('brain-container') || document.body.appendChild(document.createElement('div'));
 
-    let cube, camera, renderer, brainMesh, scene, stats, aparcSpheres, texts;
+    let cube, camera, renderer, brainMesh, scene, stats, texts, aparcNodes, eegSensors, transMatrix;
+
     let meter2centimeter = d3.scaleLinear().domain([0, 1]).range([0, 100])
 
     d3.csv('asset/fsaverage/glass-cells.csv').then((cells) => {
@@ -26,61 +37,69 @@ import normals from 'https://cdn.jsdelivr.net/npm/angle-normals@1.0.0/+esm'
 
                 for (let i in aparcRaw.name) {
                     name = aparcRaw.name[i]
-                    color = aparcRaw.color[i]
+                    color = '#fff'  // aparcRaw.color[i]
                     xyz = aparcRaw.xyz[i]
 
                     // ! The xyz map is correct, x:0, y:2, z:1
                     buffer.push({ i, name, color, x: meter2centimeter(xyz[0]), y: meter2centimeter(xyz[2]), z: meter2centimeter(xyz[1]) })
                 }
 
-                appendSpheres(buffer)
-            })
+                aparcNodes = appendSpheres(buffer)
 
-            d3.csv('asset/montage/sensor.csv').then((sensors) => {
-                sensors.map(sensor => {
-                    Object.assign(sensor, { color: '#ffffff', x: meter2centimeter(sensor.x), y: meter2centimeter(sensor.z), z: meter2centimeter(sensor.y) })
+                d3.csv('asset/montage/sensor.csv').then((sensors) => {
+                    sensors.map(sensor => {
+                        Object.assign(sensor, { color: '#ffffff', x: meter2centimeter(sensor.x), y: meter2centimeter(sensor.z), z: meter2centimeter(sensor.y) })
+                    })
+                    eegSensors = appendSpheres(sensors, 0.2)
+
+                    let loader = new FontLoader();
+
+                    loader.load(
+                        // resource URL
+                        'https://unpkg.com/three@0.158.0/examples/fonts/helvetiker_regular.typeface.json',
+
+                        // onLoad callback
+                        function (font) {
+                            // do something with the font
+                            console.log(font);
+                            appendTexts(sensors, font)
+                        },
+
+                        // onProgress callback
+                        function (xhr) {
+                            console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+                        },
+
+                        // onError callback
+                        function (err) {
+                            console.log('An error happened');
+                        }
+                    );
+
+                    transMatrix = []
+                    eegSensors.map(sensor => {
+                        transMatrix.push(aparcNodes.map(node => norm2inv(sensor.position, node.position)))
+                    })
+                    transMatrix = new Matrix(transMatrix)
+                    console.log(transMatrix)
+
                 })
-                appendSpheres(sensors)
-                console.log(sensors)
-
-                let loader = new FontLoader();
-
-                loader.load(
-                    // resource URL
-                    'https://unpkg.com/three@0.158.0/examples/fonts/helvetiker_regular.typeface.json',
-
-                    // onLoad callback
-                    function (font) {
-                        // do something with the font
-                        console.log(font);
-                        appendTexts(sensors, font)
-                    },
-
-                    // onProgress callback
-                    function (xhr) {
-                        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-                    },
-
-                    // onError callback
-                    function (err) {
-                        console.log('An error happened');
-                    }
-                );
-
             })
-
-
         })
     })
+
+    let norm2inv = (a, b) => {
+        return 1 / ((a.x - b.x) ** 2 + (a.y - b.y) ** 2 + (a.z - b.z) ** 2)
+    }
 
     /**
      * Append spheres to the scene based on the buffer.
      * @param {Array} buffer The buffer of {name, color, x, y, z}
      */
-    let appendSpheres = (buffer) => {
+    let appendSpheres = (buffer, size = 0.1, opacity = 0.8, transparent = true, flatShading = true) => {
 
-        let geometry, material, mesh, opacity = 0.8, size = 0.1, transparent = true, flatShading = true;
-        aparcSpheres = buffer.map(({ name, color, x, y, z }) => {
+        let geometry, material, mesh;
+        let meshes = buffer.map(({ name, color, x, y, z }) => {
             material = new THREE.MeshPhongMaterial({
                 color, opacity, transparent, flatShading
             })
@@ -91,10 +110,11 @@ import normals from 'https://cdn.jsdelivr.net/npm/angle-normals@1.0.0/+esm'
             return mesh
         })
 
-        aparcSpheres.map(mesh => {
+        meshes.map(mesh => {
             scene.add(mesh)
         })
 
+        return meshes
     }
 
     let appendTexts = (buffer, font) => {
@@ -105,7 +125,7 @@ import normals from 'https://cdn.jsdelivr.net/npm/angle-normals@1.0.0/+esm'
                 size: 0.5,
                 height: 0.1,
             }),
-                material = new THREE.MeshPhongMaterial({ color }),
+                material = new THREE.MeshPhongMaterial({ color, opacity: 0.3, transparent: true }),
                 mesh = new THREE.Mesh(geometry, material);
 
             Object.assign(mesh.position, { x, y, z })
@@ -139,11 +159,41 @@ import normals from 'https://cdn.jsdelivr.net/npm/angle-normals@1.0.0/+esm'
         }
 
         let animate = () => {
-            requestAnimationFrame(animate)
             cube.rotation.z += 0.01;
             cube.rotation.y += 0.01;
+
+            let v, c, vs, eegResp, ref, extent, t = performance.now() / 1000 / 2;
+            if (aparcNodes) {
+                vs = aparcNodes.map(node => {
+                    v = noise4D(node.position.x, node.position.y, node.position.z, t);
+                    c = colormap(0.5 - 0.5 * v);
+                    node.material.color.set(c);
+                    node.material.opacity = Math.abs(v);
+                    return [v]
+                })
+                vs = new Matrix(vs)
+                if (transMatrix) {
+                    eegResp = transMatrix.mmul(vs)
+                    eegResp = eegResp.data.map(d => d[0])
+                    ref = eegResp[0]
+                    eegResp = eegResp.map(d => d - ref)
+                    extent = d3.extent(eegResp)
+                    // console.log(d3.extent(eegResp))
+                    eegSensors.map((node, i) => {
+                        v = (eegResp[i] - extent[0]) / (extent[1] - extent[0])
+                        c = colormap(1 - v);
+                        node.material.color.set(c);
+                        node.material.opacity = Math.abs(v);
+                    })
+                    // return
+                }
+            }
+
             render()
+
             stats.update()
+
+            requestAnimationFrame(animate)
         }
 
         animate()
